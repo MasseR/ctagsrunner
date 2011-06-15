8368421644c994445e8d046fdb6cffa57d6d2d7f
Mats Rauhala <mats.rauhala@gmail.com>
2011-06-15 18:01:24 +0300

    Modularize

--- ctagsrunner.hs
+++ ctagsrunner.hs
@@ -5,13 +5,14 @@ import System.Directory (getDirectoryContents, doesDirectoryExist, doesFileExist
 import System.FilePath ((</>), takeExtension)
 import qualified Control.Monad.Stream as M
 import qualified Data.List.Stream as L
-import Data.Monoid(mempty)
 import qualified Data.Set as S
-import Data.Set (Set(..))
-import Control.Concurrent.MVar
+import Control.Concurrent
+import Control.Concurrent.STM
 import System.IO.Unsafe(unsafePerformIO)
+import Control.Monad (when)
+import qualified Text.PrettyPrint.Leijen as PP
 
-paths = unsafePerformIO $ newMVar S.empty
+running = unsafePerformIO $ newTVarIO False
 
 getRecursiveContents :: FilePath -> IO [FilePath]
 getRecursiveContents path = do
@@ -26,47 +27,54 @@ fields = "--fields=+iaS"
 kinds = L.map (\(l,o) -> "--" ++ l ++ "-kinds=" ++ o) langs
   where langs = [("c++", "+p")]
 ctags path = proc "ctags" ([fields] ++ kinds ++ ["-a", path])
+hasktags path = proc "hasktags" (["-c", path])
 
-getPath :: Event -> Maybe FilePath
-getPath (Modified _ path) = path >>= acceptable
-getPath (Deleted _ path ) = acceptable path
-getPath _ = mempty -- I think this is QOverFlow/DeletedSelf or maybe even Unknown
+getCtags :: FilePath -> Event -> Maybe (IO ())
+getCtags root (Modified _ path) = path >>= router root
+getCtags root (Deleted _ path ) = router root path
+getCtags _ _ = Nothing
 
 filetypes ::  [[Char]]
-filetypes = [".php"]
-acceptable :: FilePath -> Maybe FilePath
-acceptable path | takeExtension path `elem` filetypes = return path
-		| otherwise = mempty
+filetypes = [".php", ".js", ".c", ".C", ".cpp"]
+hasktypes = [".hs", ".lhs"]
+
+router :: FilePath -> FilePath -> Maybe (IO ())
+router root path | "tags" `L.isSuffixOf` path = Nothing
+                 | takeExtension path `elem` filetypes = return $ defer root path basic
+                 | takeExtension path `elem` hasktypes = return $ defer root path hask
+                 | otherwise = Nothing
+
+defer :: FilePath -> FilePath -> (FilePath -> IO ()) -> IO ()
+defer root path f = do
+  let path' = root </> path
+  doesFileExist path' >>= \e -> when e $ do
+    waitForCompletion
+    atomically $ writeTVar running True
+    putStrLn "Running ctags"
+    f path'
+    atomically $ writeTVar running False
+  where
+    waitForCompletion = atomically $ do
+      r <- readTVar running
+      if not r
+         then return ()
+         else retry
+
+basic :: FilePath -> IO ()
+basic path = createProcess (ctags path) >> return ()
+
+hask :: FilePath -> IO ()
+hask path = createProcess (hasktags path) >> return ()
 
 inotify ::  FilePath -> Event -> IO ()
-inotify root event =
-  let maybepath = getPath event
-  in case maybepath of
-	  Nothing -> return ()
-	  Just "tags" -> return ()
-	  Just path -> do
-	    putStrLn path
-	    run (root </> path)
-	    return ()
-  where
-    run path = do
-      exists <- doesFileExist path
-      if exists then modifyhandler path
-		else deletehandler path
-    deletehandler path = withMVar paths $ \set -> do
-      print "deletion handler"
-      let newset = S.delete path set
-      mapM_ (createProcess . ctags) $ S.toList newset
-      return newset
-    modifyhandler path = withMVar paths $ \set -> do
-      print "modify handler"
-      createProcess $ ctags path
-      return $ S.insert path set
+inotify root event = maybe (return ()) id $ getCtags root event
 
 main ::  IO ()
 main = withINotify $ \i -> do
   dirs <- getRecursiveContents "."
-  print dirs
+  let msg' = "Following changes in these directories:"
+      msg  = (PP.nest 1 $ L.foldr (PP.<$>) PP.empty $ L.map PP.text (msg' : dirs))
+  print msg
   M.mapM_ (\f -> addWatch i [Modify, Delete] f (inotify f)) ("." : dirs)
   getLine -- Wait for user input and quit
   return ()
